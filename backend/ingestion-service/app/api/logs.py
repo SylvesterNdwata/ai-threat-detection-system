3a8c06a5ea9820ffb187240bc6423eab9af636ca
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-03-01 14:08:09 +0100

    log collecting fastapi

--- backend/ingestion-service/app/api/logs.py
+++ backend/ingestion-service/app/api/logs.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"Hellow": "World"}
+class Log(BaseModel):
+    timestamp: datetime
+    source_ip: str
+    user_id: str | None = None
+    endpoint: str
+    status_code: int
+    user_agent: str | None = None
+    message: str
+    
+
+@app.post("/logs/")
+async def create_log(log: Log):
+    # Save the log to a database or perform any necessary processing
+    print(log)
+    return {"message": "Log saved successfully"}
+
+@app.put("logs/{log_id}")
+async def update_log(log_id: int, log: Log):
+    return {"log_id": log_id, **log.model_dump()}
